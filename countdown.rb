f86e9b0e62a97728d08b617343a1e0b99ca936ba
Angel Nunez <atlantavsop@gmail.com>
2020-02-18 21:07:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(num)
   'HAPPY NEW YEAR!'
 
 end
+
+def countdown_with_sleep(num)
+  while num > 0
+    #puts num.to_s + ' SECOND(S)!'
+    puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  'HAPPY NEW YEAR!'
+end 
